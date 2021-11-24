e7e38cc54cd8da254a5a29731eae395462d9dc0f
Chris Wiker <cwiker7@gmail.com>
2021-11-24 11:25:23 -0500

    commit tree 0ca490b34cddef3284c55e5b63e81cd55a367ee5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const superbowlWin = (array) => {
+    const obj = array.find((record => record.result === 'W'));
+    if (obj !== undefined) {
+        return obj.year;
+    }   else {
+        return undefined;
+    }
+    
+}
